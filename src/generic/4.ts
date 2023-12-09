c524ba05a92c23c681bfe8fa527bde7e23908a55
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-12-09 16:15:33 +0200

    done

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,12 +1,15 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface PageTitle {
+  title: string;
+}
 
 class Component<T> {
   constructor(public props: T) {}
 }
 
-interface Page extends Component {
+class Page extends Component<PageTitle> {
   pageInfo() {
     console.log(this.props.title);
   }
